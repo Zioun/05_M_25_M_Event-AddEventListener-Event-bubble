54541cec593ecc83fe2477b57b4572aa51229020
Zioun <jionkhan0@gmail.com>
2024-02-11 14:39:10 +0600

    added

---
+++ js/comment.js
@@ -0,0 +1,7 @@
+document.getElementById("commentBtn").addEventListener('click',function(){
+    let input = document.getElementById("input");
+    let inputValue = input.value;
+    let p = document.createElement("p");
+    p.innerText = inputValue;
+    document.getElementById("comment").appendChild(p);
+})
